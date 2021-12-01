80ba72c375db43c13e1ad38e2c987aafafc833e6
Janik9999 <44294754+Janik9999@users.noreply.github.com>
2021-12-01 15:59:28 +0100

    moin

--- Methoden/mqtt_s.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.publish as publish
- 
-
-def send(Nachricht):
-    MQTT_SERVER = "192.168.2.163"
-    MQTT_PATH = "test_c"
-    print(Nachricht)
-    publish.single(MQTT_PATH, Nachricht, hostname=MQTT_SERVER)
--- oeffnen.php
+++
@@ -1,6 +0,0 @@
-<?php
-$var1=(string)$_GET['fach'];
-exec("python3 testsend.py $var1", $output); #var1=Fach
-#print($var1);
-header('Location: http://192.168.2.150');
-?>
