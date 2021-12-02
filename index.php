388ba2ad2a5c12e1e2d546f4a45d0aaf2ed5272f
Janik9999 <44294754+Janik9999@users.noreply.github.com>
2021-12-02 10:16:35 +0100

    update

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ div {
 }
 </style>
 <?php 
+/*
 $pdo = new PDO('mysql:host=localhost;dbname=rittmeister', 'root', 'raspberry');
-sql="SELECT * FROM `rittmeistercode826`"
+$sql = "SELECT * FROM `rittmeistercode826`";
+*/
 ?>
 <head>
 	<meta charset="UTF-8">
@@ -26,45 +28,56 @@ sql="SELECT * FROM `rittmeistercode826`"
                 <th>Name</th>
                 <th>E-Mail</th>
                 <th>Code</th>
-				<th>Fach</th>
-				<th>Beginn</th>
-				<th>Ende</th>
+		<th>Fach</th>
+		<th>Beginn</th>
+		<th>Ende</th>
             </tr>
         </thead>
         <tbody>
+		
+	<tr>
+        <td>'.$row["name"].'</td>
+        <td>'.$row["mail"].'</td>
+        <td>'.$row["code"].'</td>
+		<td>'.$row["fach"].'</td>
+        <td>'.$row["start"].'</td>
+		<td>'.$row["end"].'</td>
+    </tr> 
 <?php
+/*
 	foreach ($pdo->query($sql) as $row) 
-
 {
+    
 	echo '
-			<tr>
-                <td>row["Name]</td>
-                <td>row["mail"]</td>
-                <td>row["code"]</td>
-				<td>row["fach"]</td>
-                <td>row["start"]</td>
-				<td>row["end"]</td>
-            </tr> 
+	<tr>
+        <td>'.$row["name"].'</td>
+        <td>'.$row["mail"].'</td>
+        <td>'.$row["code"].'</td>
+		<td>'.$row["fach"].'</td>
+        <td>'.$row["start"].'</td>
+		<td>'.$row["end"].'</td>
+    </tr> 
 	
 	
 	';
 }
+*/
 ?>       
         </tbody>
     </table>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="script/jquery-3.3.1.js"></script>
     <script src="dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.bootstrap4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
+    <script src="script/dataTables.bootstrap4.min.js"></script>
+    <script src="script/dataTables.buttons.min.js"></script>
+    <script src="script/buttons.bootstrap4.min.js"></script>
+    <script src="script/jszip.min.js"></script>
+    <script src="script/pdfmake.min.js"></script>
+    <script src="sricpt/vfs_fonts.js"></script>
+    <script src="sricpt/buttons.html5.min.js"></script>
+    <script src="sricpt/buttons.print.min.js"></script>
+    <script src="sricpt/buttons.colVis.min.js"></script>
+    <script src="sricpt/dataTables.responsive.min.js"></script>
+    <script src="script/responsive.bootstrap4.min.js"></script>
     <script>
 	$(document).ready(function() {
 	    var table = $('#example').DataTable( {
